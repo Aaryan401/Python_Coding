c41ca270d3c04e00f707b8b3f2747129d8623ca1
Aaryan <aaryanuttam54321@gmail.com>
2025-03-12 23:15:19 +0530

    Commit:- 12/03

---
+++ Testp1.py
@@ -0,0 +1,7 @@
+from pack1.arithmetic import add
+print("Use of Module")
+a=int(input("Enter 1st Number"))
+b=int(input("Enter 2nd Number"))
+print("1st Number=",a)
+print("2nd Number=",b)
+add(a,b)
